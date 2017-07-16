b4252d693795afe0bf0a9f38d48c09b029498576
seanfunny <seanfunny@163.com>
2017-07-16 19:12:03 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/micra-oc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/micra-oc/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/phpStudy/WWW/micra-oc/catalog/');
+define('DIR_SYSTEM', 'D:/phpStudy/WWW/micra-oc/system/');
+define('DIR_LANGUAGE', 'D:/phpStudy/WWW/micra-oc/catalog/language/');
+define('DIR_TEMPLATE', 'D:/phpStudy/WWW/micra-oc/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/phpStudy/WWW/micra-oc/system/config/');
+define('DIR_IMAGE', 'D:/phpStudy/WWW/micra-oc/image/');
+define('DIR_CACHE', 'D:/phpStudy/WWW/micra-oc/system/cache/');
+define('DIR_DOWNLOAD', 'D:/phpStudy/WWW/micra-oc/system/download/');
+define('DIR_UPLOAD', 'D:/phpStudy/WWW/micra-oc/system/upload/');
+define('DIR_MODIFICATION', 'D:/phpStudy/WWW/micra-oc/system/modification/');
+define('DIR_LOGS', 'D:/phpStudy/WWW/micra-oc/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'admin');
+define('DB_DATABASE', 'seawood');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
